8b0c5eb544558ae5792900774cb555f3143c6358
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-02-17 17:48:00 +0200

    create spring project

---
+++ backend/src/test/java/net/ivanevla/peopleapp/PeopleappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ivanevla.peopleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
